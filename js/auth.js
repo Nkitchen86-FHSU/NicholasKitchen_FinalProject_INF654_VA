e606e0b1567d552ce1ba8e0a205084b90f0fcf00
Nicholas Kitchen <njkitchen@mail.fhsu.edu>
2025-11-22 16:21:54 -0600

    Added auth.js functionality

---
+++ js/auth.js
@@ -0,0 +1,41 @@
+import { auth } from "./firebaseConfig.js";
+import {onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+import { loadTasks, syncTasks } from "./ui.js";
+
+export let currentUser = null;
+
+document.addEventListener("DOMContentLoaded", () => {
+    const logoutBtn = document.getElementById("logout-btn");
+
+    // Ensure user is authenticated
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            currentUser = user;
+            console.log("User ID: ", user.uid);
+            console.log("Email: ", user.email);
+            console.log("Name: ", user.name);
+
+            if (logoutBtn) {
+                logoutBtn.style.display = "block";
+            }
+            loadTasks();
+            syncTasks();
+        } else {
+            console.log("No user is currently signed in.");
+            window.location.href = "./pages/auth.html";
+        }
+    })
+
+    // Logout functionality
+    if (logoutBtn) {
+        logoutBtn.addEventListener("click", async () => {
+            try {
+                await signOut(auth);
+                M.toast({ html: "Logout successful!" });
+                window.location.href = "./pages/auth.html"
+            } catch (error) {
+                M.toast({ html: error.message });
+            }
+        });
+    }
+});
