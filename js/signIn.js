bc3e43d36a5923d78999241c495fd76d2edbce27
Nicholas Kitchen <njkitchen@mail.fhsu.edu>
2025-11-22 16:32:05 -0600

    Added signIn.js functionality

---
+++ js/signIn.js
@@ -0,0 +1,44 @@
+import { auth, db } from "./firebaseConfig.js";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+import { doc, setDoc } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const signInForm = document.getElementById("sign-in-form");
+    const signUpForm = document.getElementById("sign-up-form");
+    const showSignUp = document.getElementById("show-signup");
+    const showSignIn = document.getElementById("show-signin");
+    const signInBtn = document.getElementById("sign-in-btn");
+    const signUpBtn = document.getElementById("sign-up-btn");
+    console.log(signInBtn);
+
+    showSignIn.addEventListener("click", () => {
+        signUpForm.style.display = "none";
+        signInForm.style.display = "block";
+    });
+
+    showSignUp.addEventListener("click", () => {
+        signInForm.style.display = "none";
+        signUpForm.style.display = "block";
+    });
+
+    signUpBtn.addEventListener("click", async () => {
+        const name = document.getElementById("sign-up-name").value;
+        const email = document.getElementById("sign-up-email").value;
+        const password = document.getElementById("sign-up-password").value;
+
+        try {
+            const authCredential = await createUserWithEmailAndPassword(auth, email, password);
+            await updateProfile(authCredential.user, { displayName: name });
+            const docRef = doc(db, "users", authCredential.user.uid);
+            const userProperties = await setDoc(docRef, { email: email, name: name });
+
+            console.log(userProperties);
+            M.toast({ html: "Sign up successful!" });
+            window.location.href = "/";
+            signUpForm.style.display = "none";
+            signInForm.style.display = "block";
+        } catch (error) {
+            M.toast({ html: error.message });
+        }
+    });
+});
